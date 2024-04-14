730da77941eb68dfbb851c3a13af18131e4096d8
Leonid Burtsev <unoejk@gmail.com>
2024-04-14 19:47:12 +0300

    Init project

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+
+import React, { useState } from 'react'
+import './App.scss'
+import CompName from '../CompName/CompName'
+
+const App=()=>{
+    // const [takeMeName,setTakeMeName]=useState(undefined)
+    return (
+        <CompName/>
+        // <h1>Hello there</h1>
+    )
+}
+
+export default App
---
+++ src/components/CompName/CompName.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import './CompName.scss'
+
+const CompName = (props) => {
+    // const [takeMeName,setTakeMeName]=useState(undefined)
+    return (
+        <h1>Hello there</h1>
+    )
+}
+
+// takeMeName.defaultProps={
+//     takeMeName:'',
+// }
+// takeMeName.propTypes={
+//     takeMeName:(props, propName, componentName)=>{
+//         if (typeof props[propName]==='string')
+//             return null
+//         return new TypeError(`${componentName}: ${propName} must be string`)
+//     },
+// }
+
+export default CompName
